955890259d4d50aeded8cafdad955ab37f8cba0b
nicomts <>
2023-07-28 17:27:01 -0300

    Initial commit

---
+++ src/main/java/com/example/contactsrest/ContactsRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/contactsrest/ContactsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
